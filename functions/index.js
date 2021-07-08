197a39bb1dca2b321d3b8b5e5d906be5af23ea0a
KarthiK-72 <karthik.dontula63@gmail.com>
2021-07-09 02:11:24 +0530

    Yes

--- functions/index.js
+++ functions/index.js
@@ -1,25 +1,23 @@
+/* eslint-disable indent */
+/* eslint-disable object-curly-spacing */
+/* eslint-disable max-len */
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
 
+// eslint-disable-next-line no-unused-vars
 const stripe = require("stripe")(
-  {/* eslint-disable-next-line max-len*/},
+  // eslint-disable-next-line max-len
   "pk_test_51J9DOlSF7tQRNhd2bqYNFEH4q11ymWZu6NkwsAADKqdIuGA2fkNFZuTA7lC0EWuTgXMC4gIE328X3oaHGzn7mfg700PS5txIct"
 );
 
 // API
 
-
 const app = express();
 
-
-app.use(cors({origin: true}));
+app.use(cors({ origin: true }));
 app.use(express.json());
-
 
 app.get("/", (request, response) => response.status(200).send("hello world"));
 
-
 exports.api = functions.https.onRequest(app);
-
-
