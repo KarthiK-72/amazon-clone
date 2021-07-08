9640d124283005a440a6b5b82f2ca8ca2054208e
KarthiK-72 <karthik.dontula63@gmail.com>
2021-07-09 01:28:13 +0530

    final

---
+++ functions/index.js
@@ -0,0 +1,25 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+
+const stripe = require("stripe")(
+  {/* eslint-disable-next-line max-len*/},
+  "pk_test_51J9DOlSF7tQRNhd2bqYNFEH4q11ymWZu6NkwsAADKqdIuGA2fkNFZuTA7lC0EWuTgXMC4gIE328X3oaHGzn7mfg700PS5txIct"
+);
+
+// API
+
+
+const app = express();
+
+
+app.use(cors({origin: true}));
+app.use(express.json());
+
+
+app.get("/", (request, response) => response.status(200).send("hello world"));
+
+
+exports.api = functions.https.onRequest(app);
+
+
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyDTzgW_1EVwZgsbDyWONUUKktLk3jrctYY",
+  authDomain: "clone-5adaf.firebaseapp.com",
+  projectId: "clone-5adaf",
+  storageBucket: "clone-5adaf.appspot.com",
+  messagingSenderId: "674942516102",
+  appId: "1:674942516102:web:2ca6fbc70f02157a03ca07",
+  measurementId: "G-214Q4CC8SG"
+};
+
+const firebaseApp=firebase.initializeApp(firebaseConfig);
+const db=firebaseApp.firestore();
+const auth=firebase.auth();
+
+export{db,auth};
